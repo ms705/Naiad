dfe00a5bea7df877148b4ceda13d122ed96b8fa3
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-06-13 18:55:04 +0100

    Fixes to Mono build for Linux: remove WebHdfs, Storage and HdfsSupport frameworks (which work on Windows only, as they require Peloponnese.

--- build_mono.sh
+++ build_mono.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# Grab certificates necessary for NuGet
+mozroots --import --sync
+
 # To build in Debug mode. Binaries will be placed in $PROJ/bin/Debug.
 xbuild /p:Configuration="MonoDebug"
+echo "---------------------------------------------"
+echo "Mono Debug build done. For a release build, change build_mono.sh"
+echo "and uncomment the \"MonoRelease\" configuration line."
+echo "---------------------------------------------"
 
 # To build in Release mode. Binaries will be placed in $PROJ/bin/Release.
 #xbuild /p:Configuration="MonoRelease"
